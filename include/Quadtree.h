002b436f9fc86e2647518cec89e5b587987d1306
glistermelon <noah.railsbacka@gmail.com>
2024-05-10 15:53:05 -0500

    Partially implemented handle_collisions and added more stubs

--- include/Quadtree.h
+++ include/Quadtree.h
@@ -3,6 +3,29 @@
 #include <list>
 
 #include "Block.h"
+#include "Vect2.h"
+
+class Collision {
+
+    Point point1, point2;
+    Block* block1, *block2;
+
+    double total_impulse = 0; // cumulative impulse
+
+    static std::pair<Point, Point> calc_points();
+
+    static Vect2 calc_normal();
+
+    static double calc_impulse(Point p, Block* b); // impulse for point p which is on Block b
+
+public:
+
+    Collision(Block* b1, Block* b2);
+
+    void solve();
+    void apply();
+
+};
 
 class QuadtreeNode {
 
@@ -15,8 +38,6 @@ class QuadtreeNode {
 
     bool are_colliding(Block* b1, Block* b2);
 
-    // void update_block(Block*);
-
-    // void handle_collisions();
+    void handle_collisions();
 
 };
--- src/Quadtree.cpp
+++ src/Quadtree.cpp
@@ -3,3 +3,40 @@
 bool QuadtreeNode::are_colliding(Block *b1, Block *b2) { // TODO
     return false;
 }
+
+void QuadtreeNode::handle_collisions() {
+    if (top_left) top_left->handle_collisions();
+    if (top_right) top_right->handle_collisions();
+    if (bottom_left) bottom_left->handle_collisions();
+    if (bottom_right) bottom_right->handle_collisions();
+    std::vector<Collision> collisions;
+    for (Block* b1 : blocks) {
+        for (Block* b2 : blocks) {
+            if (b1 == b2) continue;
+            if (are_colliding(b1, b2))
+                collisions.push_back(Collision(b1, b2));
+        }
+    }
+}
+
+std::pair<Point, Point> Collision::calc_points() { // TODO
+    return {};
+}
+
+Vect2 Collision::calc_normal() { // TODO
+    return {};
+}
+
+double Collision::calc_impulse(Point p, Block *b) { // TODO
+    return 0;
+}
+
+Collision::Collision(Block *b1, Block *b2) : block1(b1), block2(b2) {}
+
+void Collision::solve() { // TODO
+
+}
+
+void Collision::apply() { // TODO
+
+}
