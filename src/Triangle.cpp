daea9ba98833e833169b1d6abe2b71ae39e58965
David Gu <68869959+shaply@users.noreply.github.com>
2024-05-09 23:09:44 -0500

    Centroid method complete

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -4,5 +4,5 @@
 Triangle::Triangle(Point a, Point b, Point c) : a(a), b(b), c(c) {}
 
 Point Triangle::centroid() { // TODO
-    return Point{};
+    return Point{(a.x+b.x+c.x)/3.0, (a.y+b.y+c.y)/3.0};
 }
