f1c75f7cf07abd52737dd9643f77259923519d01
David Gu <68869959+shaply@users.noreply.github.com>
2024-05-12 18:27:15 -0500

    Added screen class to handle stuff

---
+++ src/Screen.cpp
@@ -0,0 +1,15 @@
+#include "../include/Screen.h"
+
+void Screen::update() {
+    // Move blocks based on velocity
+    for (Block* block : blocks) {
+        
+    }
+
+    // Handle Collisions
+    
+    // Add gravity
+    for (Block* block : blocks) {
+        
+    }
+}
